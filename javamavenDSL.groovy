cdf93110ba8d3e8a72dfe3fafc048c49b0c10429
nicolasaceituno <106357918+nicolasaceituno@users.noreply.github.com>
2022-05-29 19:04:33 -0400

    Update javamavenDSL.groovy

--- javamavenDSL.groovy
+++ javamavenDSL.groovy
@@ -2,7 +2,7 @@ job('Java Maven APP DSL') {
   description('Java Maven con DSL para el curso de Jenkins')
   scm {
     git('https://github.com/macloujulian/simple-java-maven-app','master') { node ->
-      node / gitConfigName('Nicolas')
+      node / gitConfigName('nicolasaceituno')
       node / gitConfigEmail('nicolas.aceituno@gmail.com')
     }
   }
