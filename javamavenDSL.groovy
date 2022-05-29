e77c40437f911bf3275e715fa4a7b29896aa4588
nicolasaceituno <106357918+nicolasaceituno@users.noreply.github.com>
2022-05-29 18:53:48 -0400

    Create javamavenDSL.groovy

---
+++ javamavenDSL.groovy
@@ -0,0 +1,27 @@
+job('Java Maven APP DSL')
+  description('Java Maven con DSL para el curso de Jenkins')
+  scm {
+    git('https://github.com/macloujulian/simple-java-maven-app','master') { node ->
+      node / gitConfigName('Nicolas')
+      node / gitConfigEmail('nicolas.aceituno@gmail.com')
+    }
+  }
+  steps {
+    maven {
+      mavenInstallation('mavenjenkins')
+      goals('-B -DskipTests clean package')
+    }
+    maven {
+      mavenInstallation('mavenjenkins')
+      goals('test')
+    }
+    shell {'''
+      echo "Entrega: Desplegando la aplicacion"
+      java -jar "/var/jenkins_home/workspace/Java Maven App DSL/target/my-app-1.0-SNAPSHOT.jar"
+    '''}
+    publishers {
+      archiveArtifact('target/*.jar')
+      archiveJunit('target/surfire-reports/*.xml')
+      mailer('nicolas.aceituno@gmail.com', true, true)
+    }
+    
